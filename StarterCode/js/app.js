f457ad1f4dbc2692721f38bd9a6cd74a23395891
DESKTOP-LL56H96\marc_ <marc_e_leslie@yahoo.com>
2021-07-11 15:29:36 -0400

    Mostly done, maybe, but axis labels still half visible

--- StarterCode/js/app.js
+++ StarterCode/js/app.js
@@ -31,26 +31,29 @@ d3.csv("data.csv").then(function(yourData) {
 
     console.log(yourData); //ALL the data is present now (not just smnokes and age)
 
-    //looking at max and min for fun
-    var ageMax = d3.max(yourData, function(d) { return +d.age;} );
-    console.log(ageMax); //max age is 44.1
+    // //looking at max and min for fun
+    // var ageMax = d3.max(yourData, function(d) { return +d.age;} );
+    // console.log(ageMax); 
+    
+    // var ageMin= d3.min(yourData, function(d) { return +d.age;} );
+    // console.log(ageMin); 
 
-    var ageMin= d3.min(yourData, function(d) { return +d.age;} );
-    console.log(ageMin); //min age is 30.5
+    // var smokeMax = d3.max(yourData, function(d) { return +d.smokes;} );
+    // console.log(smokeMax); //max smoke % is 26.7
 
-    var smokeMax = d3.max(yourData, function(d) { return +d.smokes;} );
-    console.log(smokeMax); //max smoke % is 26.7
+    // var smokeMin= d3.min(yourData, function(d) { return +d.smokes;} );
+    // console.log(smokeMin); //min smoke % is 9.7
 
-    var smokeMin= d3.min(yourData, function(d) { return +d.smokes;} );
-    console.log(smokeMin); //min smoke % is 9.7
+    //max age is 44.1, min age is 30.5
+    //max smoke % is 26.7, min smoke % is 9.7
 
     //x will be smokers, y will be age
     var xLinearScale = d3.scaleLinear()
-        .domain([9, d3.max(yourData, d => d.smokes)]) //9 seems to get the circles into the right spot - 9.7 is the min smoker %
+        .domain([d3.min (yourData, d => d.smokes) -1  , d3.max(yourData, d => d.smokes) +2 ]) //9 seems to get the circles into the right spot - 9.7 is the min smoker %
         .range([0, width]);  // NO IDEA IF 0 IS THE RIGHT NUMBER
     
     var yLinearScale = d3.scaleLinear()
-        .domain([0, d3.max(yourData, d => d.age)]) //NO IDEA IF 0 IS THE RIGHT NUMBER
+        .domain([d3.min(yourData, d => d.age) -1 , d3.max(yourData, d => d.age) +2]) //NO IDEA IF 0 IS THE RIGHT NUMBER
         .range([height, 0]);  // NO IDEA IF 0 IS THE RIGHT NUMBERS 
 
     //axis functions
@@ -98,16 +101,16 @@ d3.csv("data.csv").then(function(yourData) {
     // Create axes labels
     chartGroup.append("text")
     .attr("transform", "rotate(-90)")
-    .attr("y", 0 - margin.left -6) //mess with this
+    .attr("y", 0 - margin.left -9) //mess with this
     .attr("x", 0 - (height / 2)) //this centers it
     .attr("dy", "1em")
     .attr("class", "axisText")
-    .text("Smokers (%)");
+    .text("Median Age (Years)");
 
   chartGroup.append("text")
     .attr("transform", `translate(${width / 2}, ${height + margin.top +1})`) //mess with this 
     .attr("class", "axisText")
-    .text("Median Age (years)");
+    .text("Smokers (%)");
     }).catch(function(error) {
     console.log(error);
 
